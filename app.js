ed546acb65898f0bd74b87db2a8c102869226f45
Pundlik Mane <asmita.pundlik.mane@accenture.com>
2024-12-11 13:39:58 +0530

    Episode 3 files

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div",
-    {id:"parent"},
-    [
-        React.createElement("div",{id:"child1"},
-            [
-                React.createElement("h1",{id:"heading"},"hello from child")
-            ]
-        )
-    ]
-    );
-    const getroot = ReactDOM.createRoot(document.getElementById("root"))
-    getroot.render(heading);
+// simple braces dont have return
+const Heading = () =>(
+        <h1>Hello from cpomo</h1>
+)
+
+// single line component
+const Match = () => <h1>I'm one line</h1>
+
+// currly braces have return
+const Title = () => {
+    return(
+    <React.Fragment>
+        <Heading />
+        <Match/>  {/* render a component */}
+        <Match></Match>  {/* render a component */}
+        {Match()}  {/* render a component */}
+        <h1>Hello from title</h1>
+    </React.Fragment>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root)
+root.render(<Title/>)
+
+// export function Heading(){
+//     return(
+//         <h1>Hello</h1>
+//     )
+// } 
+
